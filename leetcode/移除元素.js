d463aa0381727c4ac0bc338403955e1b6f0874e2
thinkerHope <dannygxm@qq.com>
2020-02-27 12:13:37 +0800

    移除元素

--- leetcode/移除元素.js
+++ leetcode/移除元素.js
@@ -1,1 +1,18 @@
 // 元素顺序可变
+var removeElement = function(nums, val) {
+  if (nums === null || nums.length === 0) return 0
+
+  let i = 0
+  let n = nums.length
+
+  while (i < n) {
+      if (nums[i] === val) {
+          nums[i] = nums[n - 1]
+          n--
+      } else {
+          i++
+      }
+  }
+
+  return n
+};
